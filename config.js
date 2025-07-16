0328d101bf184276db43213d156df81dabff8c1f
prm123456789 <guerrierjojo8@gmail.com>
2025-07-15 22:03:18 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+
+global.owner = ['+5511999999999']; // Ton numéro Owner
+global.sessionName = 'sessions';
+global.prefixes = ['/', '!', '.', '#']; // Multi préfixe
+global.botName = 'INCONNU XD BOT';
+global.packname = 'INCONNU XD';
+global.author = 'INCONNU BOY TECH';
